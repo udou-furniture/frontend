2874cbbc3fdfb91a53721fa393dc05e7fc720876
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-02-06 15:53:56 +1100

    refactored review form

--- src/components/Dashboard/ReviewForm.js
+++ src/components/Dashboard/ReviewForm.js
@@ -2,9 +2,11 @@ import React from 'react'
 import {Field, reduxForm} from 'redux-form'
 import {required} from '../../validations'
 
+import './ReviewForm.css'
+
 const renderTextField = ({ input, label, type, meta: { touched, error, warning } }) => (
     <div>
-        <input {...input} placeholder={label} type={type}/>
+        <textarea {...input} placeholder={label} type={type}/>
         {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
     </div>
 )
@@ -14,17 +16,19 @@ class ReviewForm extends React.Component {
         const { handleSubmit, pristine, submitting } = this.props
 
         return(
-            <form onSubmit={handleSubmit}>
-                <label>Please provide your review on our product below</label>
-                <Field 
-                        name="review" 
-                        type="text"
-                        component={renderTextField} 
-                        label="Review"
-                        validate={required}
-                    />
-                <button type="submit" disabled={pristine || submitting}>Submit</button>
-            </form>
+            <div className="review-form-grid">
+                <form className="review-form" onSubmit={handleSubmit}>
+                    <h1>Leave a review! <span className="review-tagline"></span></h1> 
+                    <Field 
+                            name="review" 
+                            type="text"
+                            component={renderTextField} 
+                            label="Review"
+                            validate={required}
+                        />
+                    <button className="review-form-button" type="submit" disabled={pristine || submitting}>Submit</button>
+                </form>
+            </div>
         )
     }
 }
